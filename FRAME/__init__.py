5289b456f5473a0a73575d5a580236c825b5084d
ShenHaotv <129112332+ShenHaotv@users.noreply.github.com>
2024-09-01 21:28:50 -0500

    Update __init__.py

--- FRAME/__init__.py
+++ FRAME/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
-from .Spatial_Digraph import SpatialDiGraph
-from .Visualization import Vis
-from .Digraphstats import Digraphstats
+from .spatial_digraph import SpatialDiGraph
+from .visualization import Vis
+from .digraphstats import Digraphstats
 
 __version__ = "1.0.0"
