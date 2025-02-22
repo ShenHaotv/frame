9ce3f2b2976994d6eafac2602ab0c14c33badddd
ShenHaotv <129112332+ShenHaotv@users.noreply.github.com>
2025-02-21 22:10:44 -0600

    Add files via upload

--- frame/lyapunov_helper.py
+++ frame/lyapunov_helper.py
@@ -32,16 +32,16 @@ def triang_lyapunov(R,F):
     y *= scale   
     return (y)
 
-def transformed_singular_lyapnov(Lp,y0,G,P,Transpose):                                           
+def transformed_singular_lyapnov(Lp,pi_hat,G,P,Transpose):                                           
     """Solve the equation LX+XL^T=G,where Transpose=False,  or L^TX+XL=G, where Transpose=True, in the least square sense, the input here has already been transformed"""   
-    d=len(y0)                                                                             
+    d=pi_hat.shape[1]                                                                            
     P1=P[:,[0]]
     P2=P[:,1:d]                                                                                                         
     L12p=Lp[[0],1:d]
     L22p=Lp[1:d,1:d]    
       
     if Transpose==False:
-       Gp=G-np.sum(np.multiply(y0@y0.T,G))*y0@y0.T                                                                      
+       Gp=G-np.sum(np.multiply(pi_hat.T@pi_hat,G))*pi_hat.T@pi_hat                                                                    
        Gp22=P2.T@Gp@P2
        Gp21=P2.T@Gp@P1 
        F=-Gp22
@@ -65,26 +65,26 @@ def transformed_singular_lyapnov(Lp,y0,G,P,Transpose):
     if Transpose==False:
        X=X-X[0,0]*np.ones((d,d))
     elif Transpose==True:  
-         X=X-(X[0,0]/(y0[[0],[0]]**2))*y0@y0.T
+         X=X-(X[0,0]/(pi_hat.T[[0],[0]]**2))*pi_hat.T@pi_hat
        
     return(X)
 
-def transformed_singular_lyapnov_rank1(Lp,y0,c,P,Transpose):                                           
+def transformed_singular_lyapnov_rank1(Lp,pi_hat,c,P,Transpose):                                           
     """Solve the equation LX+XL^T=c@c.T,where Transpose=False,  or L^TX+XL=c@c.T, where Transpose=True, in the least square sense, the input here has already been transformed"""  
-    d=len(y0)                                                                                  
+    d=pi_hat.shape[1]                                                                                
     P1=P[:,[0]]
     P2=P[:,1:d]                                                                                                         
     L12p=Lp[[0],1:d]
     L22p=Lp[1:d,1:d]    
      
     if Transpose==False:
-       yp1=y0.T@P1
-       yp2=P2.T@y0
-       product=(y0.T@c)**2                                                                  
+       pi_p1=pi_hat@P1
+       pi_p2=P2.T@pi_hat.T
+       product=(pi_hat@c)**2                                                                  
        a=P2.T@c
-       b=yp2
+       b=pi_p2
        F=-(a@a.T-product*b@b.T)
-       Gp21=(c.T@P1)*(P2.T@c)-product*yp1*yp2    
+       Gp21=(c.T@P1)*(P2.T@c)-product*pi_p1*pi_p2
        X_22p=triang_lyapunov(-L22p,F)                                                                                                                           
        X_21p=np.linalg.solve(L22p,Gp21-X_22p@L12p.T) 
  
@@ -105,7 +105,7 @@ def transformed_singular_lyapnov_rank1(Lp,y0,c,P,Transpose):
     if Transpose==False:
        X=X-X[0,0]*np.ones((d,d))
     elif Transpose==True:  
-         X=X-(X[0,0]/(y0[[0],[0]]**2))*y0@y0.T
+         X=X-(X[0,0]/(pi_hat.T[[0],[0]]**2))*pi_hat.T@pi_hat
      
     return(X)
 
@@ -114,16 +114,16 @@ def modified_singular_lyapnov(L,gamma,G,Transpose):
     d=len(gamma)
     mc=markovChain(np.identity(d)-L)
     mc.computePi('linear')
-    y=mc.pi.reshape((d,1))                                                      #Get the stationary distribution   
-    y=y/np.sum(y)
-    if np.min(y)<=0:
+    pi=mc.pi.reshape((1,d))                                                    #Get the stationary distribution   
+    pi=pi/np.sum(pi)
+    if np.min(pi)<=0:
        mc.computePi('power')
-    y=mc.pi.reshape((d,1))                                                      #Get the stationary distribution   
-    y=y/np.sum(y)
-    y0=y/np.linalg.norm(y)
-    y1=y.tolist()
+    pi=mc.pi.reshape((1,d))                                                    #Get the stationary distribution   
+    pi=pi/np.sum(pi)
+    pi_hat=pi/np.linalg.norm(pi)                                               #Normalized pi
+    pi_1=pi.flatten().tolist()                                                             
 
-    s = sorted(range(len(y1)), key=lambda k: (y1[k], k), reverse=True)          #Sort the indexes according to the sorting of y              
+    s = sorted(range(d), key=lambda k: (pi_1[k], k), reverse=True)             #Sort the indexes according to the sorting of pi              
     s_inv=np.zeros(d)
     for i in range(0,d):
         s_inv[s[i]] = i
@@ -132,13 +132,15 @@ def modified_singular_lyapnov(L,gamma,G,Transpose):
     s=s.astype(int)
     s_inv=np.array(s_inv)
     s_inv=s_inv.astype(int)
-    y=y[s]
-    y0=y0[s]
+    pi_flatten=pi.T[s]
+    pi=pi_flatten.T
+    pi_hat_flatten=pi_hat.T[s]
+    pi_hat=pi_hat_flatten.T
     L=L[s,:]
     L=L[:,s]
     G=G[s,:]
     G=G[:,s]
-    gamma=gamma[s]                                                             #Rearrange y,y0,L,G and gamma according to the sorting
+    gamma=gamma[s]                                                             #Rearrange pi,pi_hat,L,G and gamma according to the sorting
 
     P=transformer(d)   
     Lp=P.T@L@P
@@ -146,34 +148,34 @@ def modified_singular_lyapnov(L,gamma,G,Transpose):
     
     if Transpose==False:
        z=np.ones((d,1))
-       y=y  
+       pi=pi 
        (R,U)=scipy.linalg.schur(-L22p, output='real')    
     
     elif Transpose==True:
-         z=y
-         y=np.ones((d,1)) 
+         z=pi.T
+         pi=np.ones((1,d)) 
          (RT,U)=scipy.linalg.schur(-L22p.T, output='real')
          R=RT.T
   
     Z=z@z.T
-    Y=y@y.T
+    Pi=pi.T@pi
     
     Uextended=np.identity(d)
     Uextended[1:d,1:d]=U
-    Pnew=P@Uextended                                                            #Combine the real schur decomposition and computing the new transform matrix
-    Lp[1:d,1:d]=-R                                                              #Updating Lp
+    Pnew=P@Uextended                                                           #Combine the real schur decomposition and computing the new transform matrix
+    Lp[1:d,1:d]=-R                                                             #Updating Lp
     Lp[:,[0]]=np.zeros((d,1))
     Lp[[0],1:d]=Lp[[0],1:d]@U
     
-    X00=transformed_singular_lyapnov(Lp,y0,G,Pnew,Transpose)
+    X00=transformed_singular_lyapnov(Lp,pi_hat,G,Pnew,Transpose)
 
     c=np.zeros((d,1))
-    c[0]=1/y[0]
-    X01=transformed_singular_lyapnov_rank1(Lp,y0,c,Pnew,Transpose)
+    c[0]=1/pi[0,0]
+    X01=transformed_singular_lyapnov_rank1(Lp,pi_hat,c,Pnew,Transpose)
 
-    gy=np.sum(np.multiply(Y,G))                                                #Sum of entries of the hadmard product of Y and G
+    gy=np.sum(np.multiply(Pi,G))                                               #Sum of entries of the hadmard product of Y and G
     
-    X10=X00-gy*X01+gy*(1/(Y[0,0]*Z[0,0]*gamma[0]))*Z
+    X10=X00-gy*X01+gy*(1/(Pi[0,0]*Z[0,0]*gamma[0]))*Z
 
     Aux=np.zeros((d-1,d-1))                                            
     X1_=np.empty((d-1,d,d))
@@ -181,11 +183,11 @@ def modified_singular_lyapnov(L,gamma,G,Transpose):
     for j in range(2,d+1):
         c=np.zeros((d,1))
         c[j-1]=1       
-        X0j=transformed_singular_lyapnov_rank1(Lp,y0,c,Pnew,Transpose)          
-        X1j=X0j-Y[j-1,j-1]*X01+(Y[j-1,j-1]/(Y[0,0]*Z[0,0]*gamma[0]))*Z                  
+        X0j=transformed_singular_lyapnov_rank1(Lp,pi_hat,c,Pnew,Transpose)          
+        X1j=X0j-Pi[j-1,j-1]*X01+(Pi[j-1,j-1]/(Pi[0,0]*Z[0,0]*gamma[0]))*Z                  
         X1_[[j-2],:,:]=X1j                                                                           
         for i in range(1,d):
-            Aux[i-1,j-2]=gamma[i]*X1j[i,i]                                      #This corresponds to U_{gamma}^T*H1^(-1)U in the method
+            Aux[i-1,j-2]=gamma[i]*X1j[i,i]                                     #This corresponds to U_{gamma}^T*H1^(-1)U in the method
 
     a=np.identity(d-1)+Aux
     b=np.zeros((d-1,1))
@@ -200,10 +202,10 @@ def modified_singular_lyapnov(L,gamma,G,Transpose):
     for i in range(1,d):
         S+=x[[i-1],[0]]*X1_[i-1,:,:]
  
-    so=X10-S                                                                    #The solution matrix in the sorted order
+    so=X10-S                                                                   #The solution matrix in the sorted order
 
     so=so[s_inv,:]                                   
-    so=so[:,s_inv]                                                              #The solution matrix in the unsorted order                        
+    so=so[:,s_inv]                                                             #The solution matrix in the unsorted order                        
     
     so=0.5*(so+so.T)
     
