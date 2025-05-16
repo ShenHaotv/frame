1b147e8c17ebd6c5836b3180d06062c5205b9849
ShenHaotv <129112332+ShenHaotv@users.noreply.github.com>
2025-05-16 16:37:24 -0500

    Update digraphstats.py

--- frame/digraphstats.py
+++ frame/digraphstats.py
@@ -3,38 +3,6 @@ import scipy.stats as stats
 from scipy.sparse import csr_matrix
 from .loss import getindex
 
-def benjamini_hochberg(p_values, fdr=0.2):
-    """
-    Apply the Benjamini-Hochberg procedure to a list of p-values to determine significance
-    and the largest k such that p_(k) <= k/m * FDR.
-    
-    Args:
-    p_values (list or array): Array of p-values from multiple hypothesis tests.
-    fdr (float): False discovery rate threshold.
-    
-    Returns:
-    tuple:
-        array: Boolean array where True indicates the hypotheses that are accepted.
-        int: The largest k for which p_(k) <= k/m * FDR.
-    """
-    m = len(p_values)  # total number of hypotheses
-    sorted_p_values = np.sort(p_values)
-    sorted_indices = np.argsort(p_values)
-    critical_values = np.array([fdr * (i + 1) / (m) for i in range(m)])
-    
-    # Find the largest p-value that meets the Benjamini-Hochberg criterion
-    is_significant = sorted_p_values <= critical_values
-    if np.any(is_significant):
-        max_significant = np.max(np.where(is_significant)[0])  # max index where condition is true
-    else:
-        max_significant = -1  # no significant results
-    
-    # All p-values with rank <= max_significant are significant
-    significant_indices = sorted_indices[:max_significant + 1]
-    
-    # max_significant + 1 because indices are 0-based, but k should be 1-based
-    return significant_indices
-
 class Digraphstats(object):
       def __init__(self,sp_digraph):
           self.sp_digraph=sp_digraph
@@ -77,51 +45,5 @@ class Digraphstats(object):
           abs_Z_matrix=(abs_Z_triu+abs_Z_triu.T).toarray()
           ax.imshow(abs_Z_matrix, cmap='viridis',interpolation='nearest')
           ax.set_xlabel('|z-score|',fontsize=15)
-              
-      def outlier_detection(self,fdr):
-          p_value_neg=stats.norm.cdf(self.z_score)
-          p_value_pos=1-p_value_neg
-          p_values=np.minimum(p_value_pos,p_value_neg)
-          indices=benjamini_hochberg(p_values, fdr=fdr)
-          edge_index=getindex(self.D)
-          observed_indices=edge_index[indices,:]                               #Indices of edge outlier in the observed nodes                
-          h0=self.sp_digraph.h[0]
-          outlier_indices=np.zeros((len(indices),2))                           #Indices of outliers in the original digraph
-          for i in range(len(indices)):
-              outlier_indices[i,0]=h0[int(observed_indices[i,0])]
-              outlier_indices[i,1]=h0[int(observed_indices[i,1])]
-              
-          return(observed_indices, outlier_indices)
-      
-      def draw_outliers(self,
-                        v,
-                        ax,
-                        indices=None,
-                        fdr=None,
-                        draw_map=True,
-                        draw_nodes=True,):
-        
-          if fdr is None:
-             fdr=0.2
-             
-          if indices is None:
-             observed_indices,outlier_indices=self.outlier_detection(fdr)
-          
-          v.draw_migration_rates(ax,
-                                 mode='Base',
-                                 draw_map=draw_map,
-                                 draw_nodes=draw_nodes,)
-          
-          for i in range(len(outlier_indices)):
-              pair=outlier_indices[i,:]
-              pair_observed=observed_indices[i,:]
-              v.ax.plot([v.grid[int(pair[0])][0],v.grid[int(pair[1])][0]],
-                        [v.grid[int(pair[0])][1],v.grid[int(pair[1])][1]],
-                        color = 'gray', linewidth = 0.5)      
-              v.ax.text(v.grid[int(pair[0])][0],v.grid[int(pair[0])][1], str(int(pair_observed[0])),
-                        horizontalalignment="center", verticalalignment="bottom",
-                        size=v.obs_node_textsize*0.8, zorder=v.obs_node_zorder,)           
-              v.ax.text(v.grid[int(pair[1])][0],v.grid[int(pair[1])][1], str(int(pair_observed[1])),
-                        horizontalalignment="center", verticalalignment="bottom",
                         size=v.obs_node_textsize*0.8, zorder=v.obs_node_zorder,)
    
